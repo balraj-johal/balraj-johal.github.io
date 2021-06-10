51ae14b27e01cc447d88641ef4a52d7e2e4d94c3
Balraj Johal <balraj-johal@users.noreply.github.com>
2021-06-10 15:55:08 +0100

    init node index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"))
+
+app.use(express.static(path.resolve(__dirname, 'portfolio_client', 'build')));
+    
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "portfolio_client", "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`))
