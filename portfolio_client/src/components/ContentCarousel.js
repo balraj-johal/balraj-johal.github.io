c6fb25ebea73ef0516b6d2b5df3bdcf0d22ae3b4
Balraj Johal <balraj-johal@users.noreply.github.com>
2021-06-23 15:45:28 +0100

    remove bottom check for isinviewport

--- portfolio_client/src/components/ContentCarousel.js
+++ portfolio_client/src/components/ContentCarousel.js
@@ -202,7 +202,7 @@ function ContentCarousel(props) {
             return (
                 rect.top >= 0 &&
                 rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                // rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
                 rect.right <= (window.innerWidth || document.documentElement.clientWidth)
             );
         }
