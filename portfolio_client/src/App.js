d542b6646614a828a6a812cca133b4d6d122cd7b
Balraj Johal <balraj-johal@users.noreply.github.com>
2021-09-12 19:01:33 +0100

    refactor App, fix responsive conditional render

--- portfolio_client/src/App.js
+++ portfolio_client/src/App.js
@@ -11,10 +11,11 @@ import ContentCarousel from "./components/ContentCarousel"
 import Header from "./components/Header";
 
 function App() {
+    // is app at mobile resolutions?
+    let [mobile, setMobile] = useState(false);
 
     //check if we should render mobile version
-    let [mobile, setMobile] = useState(false);
-    let isMobile = () => {
+    let updateMobileState = () => {
         if (window.innerWidth <= 768) {
             setMobile(true);
         } else {
@@ -22,7 +23,8 @@ function App() {
         }
     }
     useEffect(() => {
-        window.addEventListener('resize', isMobile);
+        window.addEventListener('resize', updateMobileState);
+        // TODO: test if below if statement is redundant
         if (window.innerWidth <= 768) {
             setMobile(true);
         } else {
@@ -30,22 +32,28 @@ function App() {
         }
         
         return () => {
-            window.removeEventListener('resize', isMobile);
+            window.removeEventListener('resize', updateMobileState);
         };
     }, [])
     
-    const MOVEMENT_SCALE = 10;
+    /*
+        Transform the main site element following the mouse cursor
+
+        NOTE: only is in effect at widths > 1300px
+    */
+    const MOVEMENT_SCALER = 10;
     document.onmousemove = handleMouseMove;
-    //3D window effect
+
     function handleMouseMove(event) {
-        let target = document.getElementById("moveTarget")
+        let target = document.getElementById("transformTarget")
         const height = window.innerHeight;
         const width = window.innerWidth;
         if (width > 1300) {
+            // transform the main element with respect to mouse position/window dimensions
             let yAxisDegree = event.pageX / width * 40 - 20;
             let xAxisDegree = event.pageY / height * -1 * 40 + 20;
-            yAxisDegree = yAxisDegree/MOVEMENT_SCALE;
-            xAxisDegree = xAxisDegree/MOVEMENT_SCALE;
+            yAxisDegree = yAxisDegree/MOVEMENT_SCALER;
+            xAxisDegree = xAxisDegree/MOVEMENT_SCALER;
             target.style.transform = `rotateY(${yAxisDegree}deg) rotateX(${xAxisDegree}deg)`;
             // Set the sheen position
             setSheenPosition(event.pageX / width, event.pageY / width);
@@ -53,31 +61,67 @@ function App() {
             target.style.transform = `rotateY(0deg) rotateX(0deg)`;
         }
     }
+    // translate the sheen effect over the main element
     function setSheenPosition(xRatio, yRatio) {
         // This creates a "distance" up to 400px each direction to offset the sheen
         const xOffset = 1 - (xRatio - 0.5) * 800;
         const yOffset = 1 - (yRatio - 0.5) * 800;
-        let target = document.getElementById("moveTarget")
+        let target = document.getElementById("transformTarget")
         target.style.setProperty('--sheenX', `${xOffset}px`)
         target.style.setProperty('--sheenY', `${yOffset}px`)
     }
 
-    return (
-        <Provider store={store}>
-            <div className="app bg">
-                <div className={`main ${mobile ? "moveTargetMobileFix" : ""}`} id="moveTarget">
-                    {(mobile) ? null : <SideNav mobile={mobile}></SideNav>}
-                    <div className={`${(mobile) ? "mobile-wrap" : "right-bit" }`}>
-                        <div className="top-wrap">
-                            <Header></Header>
-                            {(mobile) ?  <TopNav /> : null}
+    if (mobile) {
+        // render mobile layout
+        return (
+            <Provider store={store}>
+                <div className="app bg">
+                    <div className="main transformTargetMobileFix" id="transformTarget">
+                        <div className="mobile-wrap">
+                            <div className="top-wrap">
+                                <Header />
+                                <TopNav />
+                            </div>
+                            <ContentCarousel />
                         </div>
-                        {(mobile) ? <ContentCarousel /> : <ContentItem />}
                     </div>
                 </div>
-            </div>
-        </Provider>
-    );
+            </Provider>
+        );
+    } else {
+        // render fullscreen layout
+        return (
+            <Provider store={store}>
+                <div className="app bg">
+                    <div className="main" id="transformTarget">
+                    <SideNav mobile={mobile}></SideNav>
+                        <div className="right-bit">
+                            <div className="top-wrap">
+                                <Header></Header>
+                            </div>
+                            <ContentItem />
+                        </div>
+                    </div>
+                </div>
+            </Provider>
+        );
+    }
+    // return (
+    //     <Provider store={store}>
+    //         <div className="app bg">
+    //             <div className={`main ${mobile ? "transformTargetMobileFix" : ""}`} id="transformTarget">
+    //                 {(mobile) ? null : <SideNav mobile={mobile}></SideNav>}
+    //                 <div className={`${(mobile) ? "mobile-wrap" : "right-bit" }`}>
+    //                     <div className="top-wrap">
+    //                         <Header></Header>
+    //                         {(mobile) ?  <TopNav /> : null}
+    //                     </div>
+    //                     {(mobile) ? <ContentCarousel /> : <ContentItem />}
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </Provider>
+    // );
 }
 
 export default App;
